c67586c8c333539f519d9ab830b7621ca1780d5d
Okhotnichenko Artur <ohotnikarturik@gmail.com>
2021-01-08 15:08:15 +0200

    add endpoints, make pages with handlerbars template, add basic style

--- src/app.js
+++ src/app.js
@@ -1,24 +1,64 @@
-const express = require('express')
+const express = require("express");
+const path = require("path");
+const hbs = require("hbs");
 
-const app = express()
+const app = express();
 
-app.get('', (req, res) => {
-  res.send("Hello express!")
-})
+// Define path for express config
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
 
-app.get('/help', (req, res) => {
-  res.send("Help page!")
-})
+// Setup handlerbars engine and views location
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
 
-app.get('/about', (req, res) => {
-  res.send("About page!")
-})
+// Setup static directory to serve for assets
+app.use(express.static(publicDirectoryPath));
 
-app.get('/weather', (req, res) => {
-  res.send("Weather page!")
-})
+app.get("/", (req, res) => {
+  res.render("index", {
+    title: "Weather app",
+    name: "Artur",
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About me",
+    name: "Artur",
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help page",
+    helpText: "If you need some help, just ask",
+    name: "Artur",
+  });
+});
+
+app.get("/weather", (req, res) => {
+  res.send({ location: "Espoo", forecast: "Mess" });
+});
+
+app.get("/help/*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Artur",
+    errorMessage: "Help article not found",
+  });
+});
+
+app.get("*", (req, res) => {
+  res.render("404", {
+    title: "404",
+    name: "Artur",
+    errorMessage: "Page not found",
+  });
+});
 
 app.listen(3000, () => {
-  console.log('Server is up on port 3000')
-  
-})
+  console.log("Server is up on port 3000");
+});
